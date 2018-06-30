8fd164fc41c214e91fbb907c483d142b5e5f8dc5
Carlos Ivan de Jesus Mendoza <de.jesusmendoza.carlosivan@gmail.com>
2018-06-30 01:57:35 -0500

    nav-bar

---
+++ js/nav-bar.js
@@ -0,0 +1,6 @@
+var btnNav = document.getElementById('btn-nav');
+var nav = document.getElementById('nav');
+
+btnNav.addEventListener('click', function() {
+    nav.classList.toggle('mostrar');
+})
